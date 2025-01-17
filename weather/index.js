a131ffb189a61cdfe23a7cf12efc5608f5476603
NawadeepKhatri123 <5086950@nwpolytech.ca>
2025-01-16 21:14:15 -0700

    mist png

--- weather/index.js
+++ weather/index.js
@@ -51,7 +51,7 @@ search.addEventListener('click', () => {
                     image.src = '../images/cloud.png';
                     break;
 
-                case 'Haze':
+                case 'Mist':
                     image.src = '../images/mist.png';
                     break;
 
